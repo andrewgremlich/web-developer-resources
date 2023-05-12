3d670d38763b3fca97577ffca7f593bee23bf86a
andrewgremlich <andrew.gremlich@gmail.com>
2023-05-12 13:02:30 -0600

    updating how data is built

---
+++ fetch-data.js
@@ -0,0 +1,81 @@
+import fs from "fs";
+import { load } from "cheerio";
+
+import { data } from "./data.js";
+
+const getUrlData = async (url) => {
+	try {
+		const page = await fetch(url);
+		const data = await page.text();
+
+		const $ = load(data);
+
+		return {
+			url,
+			title: $("title").text(),
+			description: $("meta[name='description']").attr("content"),
+		};
+	} catch {
+		return {
+			url,
+			title: url,
+			description: false,
+		};
+	}
+};
+
+const makeSection = async (sectionToGet) => {
+	const sectionData = await Promise.all(
+		sectionToGet.urls.map((url) => getUrlData(url)),
+	);
+
+	return `
+    <h2 id="${sectionToGet.title.toLowerCase().split(" ").join("-")}">${
+		sectionToGet.title
+	}</h2>
+    ${sectionData
+			.map((section) => `<p><a href="${section.url}">${section.title}</a></p>`)
+			.join("\n")}
+  `;
+};
+
+const main = (async () => {
+	const pageHtml = await Promise.all(
+		data.other.map((sectionToGet) => makeSection(sectionToGet)),
+	);
+
+	const fileToWrite = `<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>${data.title}</title>
+    <meta name="author" content="Andrew Gremlich">
+    <meta name="description" content="${data.description}">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+  </head>
+  <body>
+    <main>
+    <h1>${data.title}</h1>
+    <p>${data.description}</p>
+
+    ${pageHtml.join("")}
+    </main>
+
+    <footer>
+      <p>Last Updated: ${new Date().toLocaleDateString(undefined, {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}</p>
+    </footer>
+  </body>
+</html>`;
+
+	fs.writeFile("index.html", fileToWrite, (err) => {
+		if (err) {
+			throw err;
+		}
+		console.log("The file has been saved!");
+	});
+})();
---
+++ pull-urls.js
@@ -0,0 +1,50 @@
+import fs from "fs";
+
+// remove new line characters from readme
+const readme = fs.readFileSync("./README.md", "utf8");
+// .replace(/\n/g, "");
+
+// split read me based off h2 headers
+const splitReadme = readme.split("##");
+
+// drop first element of array
+splitReadme.shift();
+
+// a regex to select anything inbetween parenthesis
+const regex = /\(([^)]+)\)/g;
+
+// a regex to verify a URL
+const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
+
+const data = splitReadme.map((section) => {
+	const [title, ...urls] = section.split(/\n/g);
+
+	return {
+		title: title.trim(),
+		urls: urls
+			.map((url) => {
+				const match = url.match(regex);
+				if (match) {
+					return match[0].slice(1, -1).trim();
+				} else {
+					return false;
+				}
+			})
+			.filter((val) => {
+				if (val && urlPattern.test(val)) {
+					return val;
+				}
+			}),
+	};
+});
+
+const dataJSFile = `
+export const data = {
+	title: "Web Developer Resources",
+	description: "A collection of resources for a web developer",
+	other: ${JSON.stringify(data, null, 2)},
+};
+`;
+
+// write data to file
+fs.writeFileSync("./data.js", dataJSFile);
