df949019b21aa5d34e61044f8ba091ec84ddc24f
andrewgremlich <andrew.gremlich@gmail.com>
2023-09-19 12:11:36 -0600

    adding more job boards, linting, formating

--- fetch-data.js
+++ fetch-data.js
@@ -4,63 +4,69 @@ import { load } from "cheerio";
 import { data } from "./data.js";
 
 const getUrlData = async (url) => {
-  try {
-    const page = await fetch(url);
-    const data = await page.text();
-    const $ = load(data);
+	try {
+		const page = await fetch(url);
+		const data = await page.text();
+		const $ = load(data);
 
-    let title;
+		let title;
 
-    if (page.status !== 200) {
-      title = url;
-    } else {
-      title = $("title").text()
-    }
+		if (page.status !== 200) {
+			title = url;
+		} else {
+			title = $("title").text();
+		}
 
-    return {
-      url,
-      title: title,
-      description: $("meta[name='description']").attr("content"),
-    };
-  } catch {
-    return {
-      url,
-      title: url,
-      description: false,
-    };
-  }
+		return {
+			url,
+			title: title,
+			description: $("meta[name='description']").attr("content"),
+		};
+	} catch {
+		return {
+			url,
+			title: url,
+			description: false,
+		};
+	}
 };
 
 const makeSection = async (sectionToGet) => {
-  const sectionData = await Promise.all(
-    sectionToGet.urls.map((url) => getUrlData(url)),
-  );
+	const sectionData = await Promise.all(
+		sectionToGet.urls.map((url) => getUrlData(url)),
+	);
 
-  let title;
+	let title;
 
-  if (sectionToGet.title.length > 200) {
-    title = sectionToGet.title.substring(0, 200) + "...";
-  }
+	if (sectionToGet.title.length > 200) {
+		title = `${sectionToGet.title.substring(0, 200)}...`;
+	}
 
-  return `
-    <h2 id="${sectionToGet.title.toLowerCase().split(" ").join("-")
-    }">${sectionToGet.title}</h2>
+	return `
+    <h2 id="${sectionToGet.title.toLowerCase().split(" ").join("-")}">${
+		sectionToGet.title
+	}</h2>
     ${sectionData
-      .sort((a, b) => a.title.localeCompare(b.title))
-      .map((section) => `<p><a href="${section.url}">${section.title.substring(0, 100)}</a></p>`)
-      .join("\n")
-    }
+			.sort((a, b) => a.title.localeCompare(b.title))
+			.map(
+				(section) =>
+					`<p><a href="${section.url}">${section.title.substring(
+						0,
+						100,
+					)}</a></p>`,
+			)
+			.join("\n")}
   `;
 };
 
 const main = (async () => {
-  const pageHtml = await Promise.all(
-    data.other.sort((a, b) => a.title.localeCompare(b.title)).map((
-      sectionToGet,
-    ) => makeSection(sectionToGet)),
-  );
+	const pageHtml = await Promise.all(
+		data.other
+			.sort((a, b) => a.title.localeCompare(b.title))
+			.map((sectionToGet) => makeSection(sectionToGet)),
+	);
 
-  const fileToWrite = `<!DOCTYPE html>
+	const fileToWrite = `<!DOCTYPE html>
 <html lang="en-us">
   <head>
     <meta charset="utf-8">
@@ -80,19 +86,18 @@ const main = (async () => {
 
     <footer>
       <p>Last Updated: ${new Date().toLocaleDateString(undefined, {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  })
-    }</p>
+				year: "numeric",
+				month: "short",
+				day: "numeric",
+			})}</p>
     </footer>
   </body>
 </html>`;
 
-  fs.writeFile("index.html", fileToWrite, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("The file has been saved!");
-  });
+	fs.writeFile("index.html", fileToWrite, (err) => {
+		if (err) {
+			throw err;
+		}
+		console.log("The file has been saved!");
+	});
 })();
