4e6d5a26a90f7f0f38f8efe5eda0c41f802573f2
andrewgremlich <andrew.gremlich@gmail.com>
2024-04-19 13:47:48 -0600

    prisma clean up

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 export async function CreateResource({
   name,
@@ -35,21 +35,3 @@ export async function GetResourceByUrl(url: string) {
     console.error(err);
   }
 }
-
-export async function GetResources() {
-  try {
-    return await prisma.resource.findMany();
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
