c00431ce9d8f952bdc9623879f3e371fca7a7692
andrewgremlich <andrew.gremlich@gmail.com>
2024-04-19 13:48:01 -0600

    formatting

--- fetch-data.ts
+++ fetch-data.ts
@@ -5,85 +5,85 @@ import { type Resource, type ResourceDisplay, data } from "./data";
 import { CreateResource, GetResourceByUrl, prisma } from "./prisma/db";
 
 const getUrlData = async (urlToPull: string): Promise<ResourceDisplay> => {
-  const resource = await GetResourceByUrl(urlToPull);
+	const resource = await GetResourceByUrl(urlToPull);
 
-  if (resource) {
-    return {
-      url: resource.url,
-      description: resource.description ?? undefined,
-      title: resource.name,
-    };
-  }
+	if (resource) {
+		return {
+			url: resource.url,
+			description: resource.description ?? undefined,
+			title: resource.name,
+		};
+	}
 
-  try {
-    const page = await fetch(urlToPull);
-    const data = await page.text();
-    const $ = load(data);
+	try {
+		const page = await fetch(urlToPull);
+		const data = await page.text();
+		const $ = load(data);
 
-    let title: string;
+		let title: string;
 
-    if (page.status !== 200) {
-      title = urlToPull;
-    } else {
-      title = $("title").text();
-    }
+		if (page.status !== 200) {
+			title = urlToPull;
+		} else {
+			title = $("title").text();
+		}
 
-    CreateResource({
-      name: title,
-      url: urlToPull,
-      description: $("meta[name='description']").attr("content"),
-    });
+		CreateResource({
+			name: title,
+			url: urlToPull,
+			description: $("meta[name='description']").attr("content"),
+		});
 
-    return {
-      url: urlToPull,
-      title: title,
-      description: $("meta[name='description']").attr("content"),
-    };
-  } catch {
-    return {
-      url: urlToPull,
-      title: urlToPull,
-      description: undefined,
-    };
-  }
+		return {
+			url: urlToPull,
+			title: title,
+			description: $("meta[name='description']").attr("content"),
+		};
+	} catch {
+		return {
+			url: urlToPull,
+			title: urlToPull,
+			description: undefined,
+		};
+	}
 };
 
 const makeSection = async (sectionToGet: Resource) => {
-  const sectionData = await Promise.all(
-    sectionToGet.urls.map((url) => getUrlData(url))
-  );
+	const sectionData = await Promise.all(
+		sectionToGet.urls.map((url) => getUrlData(url)),
+	);
 
-  if (sectionToGet.title.length > 200) {
-    sectionToGet.title = `${sectionToGet.title.substring(0, 200)}...`;
-  }
+	if (sectionToGet.title.length > 200) {
+		sectionToGet.title = `${sectionToGet.title.substring(0, 200)}...`;
+	}
 
-  return `
+	return `
     <h2 id="${sectionToGet.title.toLowerCase().split(" ").join("-")}">${
-    sectionToGet.title
-  }</h2>
+			sectionToGet.title
+		}</h2>
     ${sectionData
-      .sort((a, b) => a.title.localeCompare(b.title))
-      .map(
-        (section) =>
-          `<p><a href="${section.url}">${section.title.substring(
-            0,
-            100
-          )}</a></p>`
-      )
-      .join("\n")}
+			.sort((a, b) => a.title.localeCompare(b.title))
+			.map(
+				(section) =>
+					`<p><a href="${section.url}">${section.title.substring(
+						0,
+						100,
+					)}</a></p>`,
+			)
+			.join("\n")}
   `;
 };
 
 const main = (async () => {
-  const pageHtml = await Promise.all(
-    data.other
-      .sort((a, b) => a.title.localeCompare(b.title))
-      .map((sectionToGet) => makeSection(sectionToGet))
-  );
+	const pageHtml = await Promise.all(
+		data.other
+			.sort((a, b) => a.title.localeCompare(b.title))
+			.map((sectionToGet) => makeSection(sectionToGet)),
+	);
 
-  await prisma.$disconnect();
+	await prisma.$disconnect();
 
-  const fileToWrite = `<!DOCTYPE html>
+	const fileToWrite = `<!DOCTYPE html>
   <html lang="en-us">
     <head>
       <meta charset="utf-8">
@@ -103,18 +103,18 @@ const main = (async () => {
 
       <footer>
         <p>Last Updated: ${new Date().toLocaleDateString(undefined, {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        })}</p>
+					year: "numeric",
+					month: "short",
+					day: "numeric",
+				})}</p>
       </footer>
     </body>
   </html>`;
 
-  fs.writeFile("index.html", fileToWrite, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("The file has been saved!");
-  });
+	fs.writeFile("index.html", fileToWrite, (err) => {
+		if (err) {
+			throw err;
+		}
+		console.log("The file has been saved!");
+	});
 })();
