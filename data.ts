a297d79b687d881e85b81869c9c26d1b34523826
andrewgremlich <andrew.gremlich@gmail.com>
2024-05-03 10:13:56 -0600

    Adding in stream

--- data.ts
+++ data.ts
@@ -269,6 +269,7 @@ export const data: Resources = {
 				"https://github.com/public-apis/public-apis",
 				"https://www.utf8-chartable.de/unicode-utf8-table.pl",
 				"https://www.unicode.org/charts/PDF/U0000.pdf",
+				"https://getstream.io/",
 			],
 		},
 		{
