19fc55d544aa5b5f90c55185f851124b902bca8a
andrewgremlich <andrew.gremlich@gmail.com>
2024-07-20 20:06:40 -0600

    adding a checkbox reset

--- hash-listener.js
+++ hash-listener.js
@@ -1,5 +1,11 @@
+const allCheckboxes = document.querySelectorAll("input[type='checkbox']");
+
 document.addEventListener("DOMContentLoaded", () => {
   const urlHash = window.location.hash;
+
+  for (const checkbox of allCheckboxes) {
+    checkbox.checked = false;
+  }
 
   if (urlHash) {
     const hashedElement = document.querySelector(urlHash);
