8ba21d88f0cd62fae93af811daf4d419895f4481
andrewgremlich <andrew.gremlich@gmail.com>
2025-04-07 11:49:46 -0600

    added 3d resource

--- data.ts
+++ data.ts
@@ -1002,6 +1002,7 @@ export const data: Resources = {
 				"https://maps.planet.fu-berlin.de/#map=3/2074498.35/0",
 				"https://stuffin.space/",
 				"https://skia.org/docs/user/modules/canvaskit/",
+				"https://animejs.com/",
 			],
 		},
 		{
