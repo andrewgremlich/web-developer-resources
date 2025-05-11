366c4242e756f3c3614210c5f47575a691c24d7b
andrewgremlich <andrew.gremlich@gmail.com>
2025-05-11 16:27:25 -0600

    adding veracrypt

--- data.ts
+++ data.ts
@@ -452,6 +452,7 @@ export const data: Resources = {
 				"https://joindeleteme.com/",
 				"https://matrix.org/",
 				"https://atproto.com/",
+				"https://www.veracrypt.fr/en/Home.html",
 			],
 		},
 		{
