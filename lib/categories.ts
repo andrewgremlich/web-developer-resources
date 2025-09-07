b05a49c1c252dbced693aaafbb5f1db965ded5d7
andrewgremlich <andrew.gremlich@gmail.com>
2025-09-07 17:11:16 -0600

    adding cache option

--- lib/categories.ts
+++ lib/categories.ts
@@ -1,3 +1,5 @@
 export function fetchBookmarks() {
-  return fetch(`/api/v1/bookmarks`).then((res) => res.json());
+  return fetch(`/api/v1/bookmarks`, { cache: "force-cache" }).then((res) =>
+    res.json()
+  );
 }
